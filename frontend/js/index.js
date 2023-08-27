86f2521116cdf36f6b1ba6375f14db10b240b070
ramkumarperaman <ramkumar777p@gmail.com>
2023-08-27 22:29:41 +0530

    new changes

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,61 @@
-const scrollButton = document.getElementById("scrollButton");
-const scrollableContent = document.getElementById("scrollableContent");
+document.addEventListener("DOMContentLoaded", function() {
+  const staffLink = document.getElementById("openStaff");
+  const staffOffcanvas = new bootstrap.Offcanvas(document.getElementById("staffOffcanvas"));
 
-scrollButton.addEventListener("click", () => {
-  scrollableContent.classList.toggle("expanded");
+  staffLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      staffOffcanvas.show();
+  });
 });
+
+  //  HOD
+document.addEventListener("DOMContentLoaded", function() {
+  const hodLink = document.getElementById("openHOD");
+  const hodOffcanvas = new bootstrap.Offcanvas(document.getElementById("hodOffcanvas"));
+
+  hodLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      hodOffcanvas.show();
+  });
+
+  const staffLink = document.getElementById("openStaff");
+  const staffOffcanvas = new bootstrap.Offcanvas(document.getElementById("staffOffcanvas"));
+
+  staffLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      staffOffcanvas.show();
+  });
+});
+
+
+
+
+
+function validateForm() {
+  const idOrEmail = document.getElementById("idOrEmail").value;
+  const password = document.getElementById("password").value;
+  const passwordError = document.getElementById("passwordError");
+  
+  if (idOrEmail.includes("@")) {
+    // Validate email
+    if (!isValidEmail(idOrEmail)) {
+      passwordError.textContent = "";
+      alert("Invalid email format.");
+      return false;
+    }
+  }
+  
+  if (password.length < 7) {
+    passwordError.textContent = "Password must be at least 7 characters long.";
+    return false;
+  }
+  
+  passwordError.textContent = "";
+  return true;
+}
+
+function isValidEmail(email) {
+  // Very basic email validation
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailPattern.test(email);
+}
